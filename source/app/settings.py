d6861c804a23f71558fa6e76023041bdd1ed69ea
YerbolZhulniyazov <zhulniyazov@gmail.com>
2023-04-17 22:19:59 +0600

    Обновлены настройки проекта
    Добавлено приложение api и Django REST Framework (DRF)

--- source/app/settings.py
+++ source/app/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'django_bootstrap5',
     "webapp",
-    "accounts"
+    "accounts",
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
